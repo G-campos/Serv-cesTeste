49f3a38b75b718c11743c32bc449574e6953d35b
kontrolbits <gabodecampos@gmail.com>
2020-12-04 19:40:22 -0300

    update, dados estao estaticos por enquanto

--- client/src/layouts/MyLayout.vue
+++ client/src/layouts/MyLayout.vue
@@ -3,7 +3,7 @@
 
     <q-header reveal elevated class="text-white" height-hint="98" style="background: linear-gradient(to right, rgba(112, 54, 119, 1) 0%, rgba(241, 121, 27, 1) 100%);">
       <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="left = !left" />
+        <q-btn dense flat rounded icon="menu" @click="left = !left" />
 
         <q-toolbar-title>
           <q-avatar>
@@ -22,14 +22,46 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="left" side="left" overlay bordered style="background: linear-gradient(to right, rgba(112, 54, 119, 1) 0%, rgba(241, 121, 27, 1) 100%);">
-
+    <q-drawer v-model="left" side="left" overlay bordered content-class="grad">
+      <q-list>
+        <div class="q-pa-md text-white" style="max-width: 350px">
+          <q-list>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="home" />
+              </q-item-section>
+              <q-item-section>
+                HOME
+              </q-item-section>
+            </q-item>
+            <q-expansion-item expand-separator icon="assignment" label="CADASTROS" default-closeed expand-icon-class="text-white">
+              <q-list padding>
+                <q-item clickable v-ripple>
+                  <q-item-section avatar>
+                    <q-icon name="add_to_photos" />
+                  </q-item-section>
+                  <q-item-section>
+                    Conteúdo
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-expansion-item>
+          </q-list>
+        </div>
+      </q-list>
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
 
+    <q-footer reveal class="footer grad text-white" style="border-radius: 0px">
+      <q-toolbar class="flex flex-center">
+        <p style="text-align: center">Coporight© <strong> <a> 2020 - Dashboard®. </a></strong> Todos os direitos reservados.
+          Desenvolvido por <a href="https://github.com/kontrolbits" style="color:rgba(112, 54, 119, 1) "><strong>Kontrol Bits</strong></a>
+        </p>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
@@ -44,3 +76,17 @@ export default {
   }
 }
 </script>
+<style>
+.footer {
+    position: absolute;
+}
+.grad {
+    background: linear-gradient(to right, rgba(112, 54, 119, 1) 0%, rgba(241, 121, 27, 1) 100%);
+}
+.grad-invertido {
+    background: linear-gradient(to left, rgba(112, 54, 119, 1) 0%, rgba(241, 121, 27, 1) 100%);
+}
+.meu-icone {
+    color: rgba(241, 121, 27, 1);
+}
+</style>
--- client/src/pages/Login.vue
+++ client/src/pages/Login.vue
@@ -1,51 +1,32 @@
 <template>
-  <q-layout view="hHh LpR lFf">
+<q-layout view="hHh LpR lFf">
     <q-page-container>
-      <q-page class="flex flex-center">
-        <div class="q-gutter-md">
-          <q-form @submit="onSubmit">
-            <q-input
-              class="q-pb-md"
-              v-model="email"
-              rounded
-              outlined
-              type="email"
-              label="Email"
-              autofocus
-            />
-            <q-input
-              v-model="password"
-              rounded
-              outlined
-              :type="isPwd ? 'password' : 'text'"
-              label="Senha"
-            >
-              <template v-slot:append>
-                <q-icon
-                  :name="isPwd ? 'visibility_off' : 'visibility'"
-                  class="cursor-pointer"
-                  @click="isPwd = !isPwd"
-                />
-              </template>
-            </q-input>
-            <div class="row justify-center q-pt-md">
-              <q-btn
-                rounded
-                label="Entrar"
-                type="submit"
-                color="primary"
-                v-close-popup
-              />
+        <q-page class="flex flex-center" style="background: linear-gradient(to right, rgba(112, 54, 119, 1) 0%, rgba(241, 121, 27, 1) 100%);">
+            <div class="q-gutter-md text-gray">
+                <q-form>
+                    <div class="row justify-center q-pt-md">
+                      <p class="text-h3 text-gray">Login</p>
+                    </div>
+                    <q-input class="q-pb-md" v-model="email" rounded outlined type="email" label="Email" autofocus />
+                    <q-input v-model="password" rounded outlined :type="isPwd ? 'password' : 'text'" label="Senha">
+                        <template v-slot:append>
+                            <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd" />
+                        </template>
+                    </q-input>
+                    <div class="row justify-center q-pt-md">
+                        <q-btn rounded label="Entrar" type="submit" to="/"/>
+                    </div>
+                </q-form>
             </div>
-          </q-form>
-        </div>
-      </q-page>
+        </q-page>
     </q-page-container>
-  </q-layout>
+</q-layout>
 </template>
 
 <script>
-import { HTTPClient } from '../boot/axios'
+// import {
+//   HTTPClient
+// } from '../boot/axios'
 
 export default {
   name: 'Login',
@@ -55,9 +36,6 @@ export default {
       password: '',
       isPwd: true
     }
-  },
-  created () {
-    console.log(HTTPClient)
   }
 }
 </script>
@@ -65,4 +43,5 @@ export default {
 <style lang="sass" scoped>
 .my-custom-toggle
   border: 1px solid #027be3
+
 </style>
